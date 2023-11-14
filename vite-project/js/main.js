039e0326f926c7c2227f4e9f8be6d6779cc5d7f3
pintongc <pintongc@nycstudents.net>
2023-11-14 09:21:32 -0500

    5

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -7,4 +7,12 @@ const button = {
     button: document.querySelector(".switch"),
 };
 
-button.button.addEventListener("submit",)
+document.querySelector(".switch").addEventListener("click", function(){
+    if(document.body.classList.contains("dark")){
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+    } else {
+        document.body.classList.add("dark")
+        document.body.classList.remove("light");
+    }
+});
